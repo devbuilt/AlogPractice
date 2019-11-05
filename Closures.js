5446a9182ec470a88820550257dbecb5dd2cd090
Devbuilt <leo@devbuiltlabs.com>
2019-11-05 09:44:57 -0800

    getUniqueId

--- Closures.js
+++ Closures.js
@@ -1,7 +1,25 @@
 // Closure is a function that access outside its self for example
 const message = 'The British are coming!';
     function sayMessage(){
-        console.log(message)//?
+        alert(message)//?
         // Here we have access to message,
         // even though it's declared outside this function!
     }
+
+//Function for getting the id of a dom element,
+//giving it a new, unique id if it doesn't have an id yet
+
+const getUniqueId = (() => {
+let nextGeneratedId = 0;
+return element => {
+    if(!element.id){
+        element.id = `generated-uid -${nextGeneratedId}`;
+        nextGeneratedId++;
+    }
+    return element.id
+    }
+})();
+
+//Why did we put nextGeneratedId in an immediately-executed anonymous
+//function? It makes nextGeneratedId private, which prevents accidental changes 
+//from the outside world:
