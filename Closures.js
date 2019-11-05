11667df0f2549fd85e9ee649c373a43f646f1706
Devbuilt <leo@devbuiltlabs.com>
2019-11-05 10:00:25 -0800

    finished clousers

--- Closures.js
+++ Closures.js
@@ -1,10 +1,11 @@
 // Closure is a function that access outside its self for example
-const message = 'The British are coming!';
+const message = 'The British are coming!';//?
     function sayMessage(){
         alert(message)//?
         // Here we have access to message,
         // even though it's declared outside this function!
     }
+
 
 //Function for getting the id of a dom element,
 //giving it a new, unique id if it doesn't have an id yet
@@ -13,13 +14,33 @@ const getUniqueId = (() => {
 let nextGeneratedId = 0;
 return element => {
     if(!element.id){
-        element.id = `generated-uid -${nextGeneratedId}`;
+        element.id = `generated-uid-${nextGeneratedId}`;
         nextGeneratedId++;
     }
-    return element.id
+    return element.id//?
     }
 })();
 
 //Why did we put nextGeneratedId in an immediately-executed anonymous
 //function? It makes nextGeneratedId private, which prevents accidental changes 
 //from the outside world:
+
+// Function for getting the id of a dom element, 
+// giving it a new, unique id if it doesnt have the id yet
+
+let nextGeneratedId = 0;
+const qetUniqueId = element => {
+    if(!element.id){
+        element.id = `generaged-uid-${nextGeneratedId}`;
+        nextGeneratedId++;
+    }
+    return element.id;
+
+    //.. 
+    // Somewhere else in the codebase
+    //..
+
+    // WHOOPS--FORGOT I WAS ALREADY USING THIS FOR SOMETHING
+}
+
+nextGeneratedId = 0;//?
