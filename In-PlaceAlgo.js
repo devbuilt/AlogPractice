b9d508881cc5e31cb57a215feac62bf1825401c4
Devbuilt <leo@devbuiltlabs.com>
2019-11-07 10:24:26 -0800

    in place algorithms

--- In-PlaceAlgo.js
+++ In-PlaceAlgo.js
@@ -1,9 +1,38 @@
-function squareArray(intArray){
-    intArray.forEach(int,index => {
+function squareArrayInPlace(intArray){
+
+    intArray.forEach((int,index) => {
         intArray[index] *= int;
+    
     });
 
     //NOTE: no need to return anything - we modified
     //intArray in place
 }
 
+
+
+function squareArrayOutOfPlace(intArray){
+    
+    //we allocate a new array that we'll fill in with the new values
+
+    const squaredArray = [];
+
+    intArray.forEach((int, index) => {
+        squaredArray[index] = Math.pow(int, 2)//?
+   
+    });
+
+    return squaredArray;//?
+
+}
+
+squareArrayOutOfPlace([67])
+
+
+const originalArray = [2,3,4,5];
+
+squareArrayInPlace(originalArray);
+
+console.log('original array: ' + originalArray)
+
+//logs: original array 4,9,16,25 confusingly
